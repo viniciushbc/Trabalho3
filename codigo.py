a01127d811ee293d924620042c76a4da5480d176
Vini <100978033+viniciushbc@users.noreply.github.com>
2023-06-20 22:12:34 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,9 +1,8 @@
-vetor = ['x','y','z'] # Matriz "vetor" que armazena strings, no caso 'x', 'y' e 'z'.
-x = 'r' in vetor # Verificação para descobrir se 'r' está armazenada na matriz "vetor", caso verdadeiro, a variável x obterá o valor True, caso contrário x obtera o valor False. Neste caso, x = True
-
-if x is True: # Verificação do valor da variável x, neste caso, se x for verdadeiro, o programa executará a linha 5.
-  print("Correto") # O programa imprime a palavra "Correto".
-elif x is not False: # Outra condicional igual a linha 4, verificando se x é verdadeiro. Caso x = True, o programa executa a linha 7. (Essa linha de código só é executada caso o teste da linha 4 retornar x = False)
-  print("Continua Correto") # O programa imprime as palavras "Continua Correto".
-else: # Caso as condições não sejam satisfeitas, essa linha dá o comando para o programa executar a linha 9. (Essa linha de código só é executada quando os testes anteriores retornarem x = False)
-  print("Errado") # O programa imprime a palavra "Errado".
+import cv2  # Importa a biblioteca cv2, que nos permite trabalhar com imagens
+import numpy as np  # Importa a biblioteca numpy, que nos permite fazer cálculos com números
+img = cv2.imread('j.png', 0) # O codigo lê a imagem 'j.png' em escala de cinza
+img_opening = cv2.imread('j_ruido.png', 0) # O código lê a imagem 'j_ruido.png' em escala de cinza
+img_closing = cv2.imread('j_furos.png', 0) # O código lê a imagem 'j_furos.png' também em escala de cinza
+altura, largura = img.shape # Obtém a altura e largura da imagem 'img'
+kernel = np.ones((5, 5), np.uint8) # Cria uma matriz 5x5 preenchida com números um
+print(kernel) # Imprime a matriz
